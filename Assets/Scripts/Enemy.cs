035e29cf129ef3baa4a31ab851a25e82b314cecf
TanHoang01 <tanhm2k1@gmail.com>
2024-06-27 13:11:39 +0700

    add more enemy and fix timeline

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,21 +5,57 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] ParticleSystem enemyExplosion;
-    [SerializeField] Transform spawnPosition;
+    [SerializeField] ParticleSystem hitEffect;    
     [SerializeField] int score;
+    [SerializeField] int hitPoint;
 
+    Transform spawnPosition;
     ScoreBoard scoreBoard;
 
     private void Start()
     {
+        spawnPosition = GameObject.FindWithTag("SpawnAtRuntime").transform;
         scoreBoard = FindAnyObjectByType<ScoreBoard>();
+        CreateRigibody();
+    }
+
+    private void CreateRigibody()
+    {
+        Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+        rb.useGravity = false;
     }
 
     private void OnParticleCollision(GameObject other)
     {
+        CreateHitEffect();
+        CountScoreBoard();
+        DestroyMySelf();
+    }
+
+    private void CountScoreBoard()
+    {
+        scoreBoard.IncreaseScore(score);
+    }
+
+    private void DestroyMySelf()
+    {
+        hitPoint -= 1;
+        if (hitPoint < 0)
+        {
+            Destroy(gameObject);
+            CreateDeathEffect();
+        }           
+    }
+
+    private void CreateDeathEffect()
+    {
         ParticleSystem vfx = Instantiate(enemyExplosion, transform.position, Quaternion.identity);
         vfx.transform.parent = spawnPosition;
-        scoreBoard.IncreaseScore(score);
-        Destroy(gameObject);
+    }
+
+    private void CreateHitEffect()
+    {
+        ParticleSystem vfx = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        vfx.transform.parent = spawnPosition;
     }
 }
